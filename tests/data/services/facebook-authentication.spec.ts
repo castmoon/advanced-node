09056ffdc8f11c52e631660bf74dbcf39fc5a565
Guilherme Alves <academicgmag@gmail.com>
2022-06-24 16:27:53 -0300

    simplify token code

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -5,6 +5,7 @@ import { AuthenticationError } from '@/domain/errors';
 describe('Facebook authentication service', () => {
   let sut: FacebookAuthenticationService;
   let loadFacebookApi: LoadFacebookUserApi;
+  const token = 'any_token';
   beforeEach(() => {
     loadFacebookApi = {
       loadUserBy: jest.fn()
@@ -13,16 +14,16 @@ describe('Facebook authentication service', () => {
   });
 
   test('should call LoadFacebookUserApi with correct params', async () => {
-    await sut.perform({ token: 'any_token' });
+    await sut.perform({ token });
 
-    expect(loadFacebookApi.loadUserBy).toHaveBeenCalledWith({ token: 'any_token' });
+    expect(loadFacebookApi.loadUserBy).toHaveBeenCalledWith({ token });
     expect(loadFacebookApi.loadUserBy).toHaveBeenCalledTimes(1);
   });
 
   test('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
     loadFacebookApi.loadUserBy = async () => undefined;
 
-    const authResult = await sut.perform({ token: 'any_token' });
+    const authResult = await sut.perform({ token });
 
     expect(authResult).toEqual(new AuthenticationError());
   });
