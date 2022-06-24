bdcf740327009f5f5a11ad9921c67cc82fd9987b
Guilherme Alves <academicgmag@gmail.com>
2022-06-24 16:48:30 -0300

    ensure Service calls LoadUserAccountRepo when LoadFacebookUserApi returns data

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,14 +1,21 @@
 import { FacebookAuthentication } from '@/domain/features';
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 import { AuthenticationError } from '@/domain/errors';
+import { LoadUserAccountRepository } from '../contracts/repos';
 
 export class FacebookAuthenticationService implements FacebookAuthentication {
-  constructor (private readonly loadFacebookByUserTokenApi: LoadFacebookUserApi) {}
+  constructor (
+    private readonly loadFacebookByUserTokenApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepo: LoadUserAccountRepository
+  ) {}
+
   async perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
     const result = await this.loadFacebookByUserTokenApi.loadUserBy({ token: params.token });
     if (result === undefined) {
       return new AuthenticationError();
     }
+
+    await this.loadUserAccountRepo.load({ email: result.email });
     return {
       accessToken: ''
     };
--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,16 +1,24 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
+import { LoadUserAccountRepository } from '@/data/contracts/repos';
 import { FacebookAuthenticationService } from '@/data/services/';
 import { AuthenticationError } from '@/domain/errors';
 
 describe('Facebook authentication service', () => {
   let sut: FacebookAuthenticationService;
   let loadFacebookApi: LoadFacebookUserApi;
+  let loadUserAccountRepo: LoadUserAccountRepository;
   const token = 'any_token';
   beforeEach(() => {
     loadFacebookApi = {
       loadUserBy: jest.fn()
     };
-    sut = new FacebookAuthenticationService(loadFacebookApi as any);
+    loadUserAccountRepo = {
+      load: jest.fn()
+    };
+    sut = new FacebookAuthenticationService(
+      loadFacebookApi,
+      loadUserAccountRepo
+    );
   });
 
   test('should call LoadFacebookUserApi with correct params', async () => {
@@ -26,5 +34,18 @@ describe('Facebook authentication service', () => {
     const authResult = await sut.perform({ token });
 
     expect(authResult).toEqual(new AuthenticationError());
+  });
+
+  test('should call LoadUserAccountRepo when LoadFacebookUserApi returns data', async () => {
+    loadFacebookApi.loadUserBy = async () => ({
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookID: 'any_fb_facebook_id'
+    });
+
+    await sut.perform({ token });
+
+    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({ email: 'any_fb_email' });
+    expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1);
   });
 });
